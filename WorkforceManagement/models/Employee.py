9b72880335ceb47aa883132c4cd9cfcb3491f291
CashewRose <danieagnoletti@gmail.com>
2018-08-06 14:58:26 -0500

    Fixed comma typo

--- WorkforceManagement/models/Employee.py
+++ WorkforceManagement/models/Employee.py
@@ -6,7 +6,7 @@ class Employee(models.Model):
     """
 
     department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    computer_id = models.ForeignKey(Computer. on_delete=models.CASCADE)
+    computer_id = models.ForeignKey(Computer, on_delete=models.CASCADE)
     is_supervisor = models.BooleanField(default=False)
     name = models.CharField(max_length="50")
     start_date = models.DateField(auto_now_add=True)
