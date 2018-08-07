b7b63757b60cdd428b4b1c344934ecd05d60bc83
dan beecroft <binauralbeat001@gmail.com>
2018-08-07 15:38:41 -0500

    working on templates

--- WorkforceManagement/urls.py
+++ WorkforceManagement/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('/computers/', views.Computer_List_View, name="computer_list"),
     path('/computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
     path('/computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
-    path('/training_progs/', views.Training_List_View, name="training_prog_list"),
+    path('/training_programs/', views.Training_List_View, name="training_list"),
 ]
