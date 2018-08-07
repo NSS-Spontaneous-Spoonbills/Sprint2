92c03c5bce6ff5f18a37be21079ef5b8b6874133
David Paul <dfp333@gmail.com>
2018-08-07 10:10:32 -0500

    checking out pr

--- WorkforceManagement/views/Department_View.py
+++ WorkforceManagement/views/Department_View.py
@@ -2,4 +2,5 @@ from django import HttpResponse
 
 def Department_List_View(request):
     """ Displays all Departments in the company """
-    departments =
+    departments = Departments.objects.all()
+    return render(request, 'WorkforceManagement/Department_list.html', {'departments': departments})
