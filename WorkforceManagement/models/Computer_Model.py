62dec134c7c7c9f2f3831e8ff6fd80eaa19b7495
MrErin <erinmeaker@gmail.com>
2018-08-07 10:07:50 -0500

    computer list view and associated templates working

--- WorkforceManagement/models/Computer_Model.py
+++ WorkforceManagement/models/Computer_Model.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Computer(models.Model):
     """models data for bangazon employee computers"""
-    id = models.AutoField(primary=True)
     purchase_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     decom_date = models.DateTimeField(auto_now=True, auto_now_add=False)
     manufacturer = models.CharField(max_length=50)
     make = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"ComputerNum_something"
---
+++ WorkforceManagement/views/Computer_View.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from WorkforceManagement.models import Computer
+
+
+def Computer_List_View(request):
+    """Displays all computers in the database"""
+    computers = Computer.objects.all()
+    return render(request, 'WorkforceManagement/Computer_List.html', {'computers': computers})
