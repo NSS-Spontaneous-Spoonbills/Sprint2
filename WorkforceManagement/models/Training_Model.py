8cfc19946dee7bc0ff8c7087a6b294d1eec0dfc0
dan beecroft <binauralbeat001@gmail.com>
2018-08-07 09:56:07 -0500

    created model for Training_Model.py

---
+++ WorkforceManagement/models/Training_Model.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Training_Prog(models.Model):
+    """
+    Model for Training Table contains training_name, training_desc(ription), start_date
+    end_date, max_attendance.
+    """
+
+    prog_name = models.CharField(max_length="50")
+    training_desc = models.CharField(max_length="100")
+    start_date = models.DateField(auto_now=True)
+    end_date = models.DateField(auto_now=False)
+    max_attendance = models.CharField(max_length="50")
+
+    def __str__(self):
+
+        return f'{self.name}'
