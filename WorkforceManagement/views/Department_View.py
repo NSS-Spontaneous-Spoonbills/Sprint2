8b592d61da46246a78846e32a6c15ed1ecd227ad
David Paul <dfp333@gmail.com>
2018-08-07 10:33:50 -0500

    added imort to views

--- WorkforceManagement/views/Department_View.py
+++ WorkforceManagement/views/Department_View.py
@@ -1,6 +1,7 @@
 from django import HttpResponse
+from WorkforceManagement.models import Department
 
 def Department_List_View(request):
     """ Displays all Departments in the company """
-    departments = Departments.objects.all()
+    departments = Department.objects.all()
     return render(request, 'WorkforceManagement/Department_list.html', {'departments': departments})
