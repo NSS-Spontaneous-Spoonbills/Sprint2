2c849d72f4283f4adaaee7033d42167d4d065c06
CashewRose <danieagnoletti@gmail.com>
2018-08-08 11:08:07 -0500

    Now connects to the correct template. Still need to show correct specific data.

--- WorkforceManagement/urls.py
+++ WorkforceManagement/urls.py
@@ -5,7 +5,7 @@ from WorkforceManagement.views import Employee_View
 
 urlpatterns = [
     path('/computers/', views.Computer_List_View, name="computer_list"),
-    path('/employees/', views.Employee_View.Employee_List_View.as_view(), name='Employee_List'),
+    path('/employees/', views.Employee_View.Employee_List_View.as_view(), name='employee_list'),
     path('/employees/<int:pk>', views.Employee_View.Employee_Detail_View.as_view(), name='employee_detail'),
     path('/computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
     path('/training_progs/', views.Training_List_View, name="training_prog_list"),
--- WorkforceManagement/views/Employee_View.py
+++ WorkforceManagement/views/Employee_View.py
@@ -5,7 +5,9 @@ from WorkforceManagement.models import Department, Computer, Employee
 class Employee_List_View(ListView):
     model = Employee
     context_object_name = 'Employee_List'
+    template_name = 'WorkforceManagement/Employee_List.html'
 
 class Employee_Detail_View(ListView):
     model = Employee
     context_object_name = 'Employee_Detail'
+    template_name = 'WorkforceManagement/Employee_Detail.html'
