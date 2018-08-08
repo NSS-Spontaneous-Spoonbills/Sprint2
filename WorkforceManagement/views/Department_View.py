27136a392276e3af5fb016c12ec2abb8a119689a
David Paul <dfp333@gmail.com>
2018-08-08 11:07:18 -0500

    updated detail view methods

--- WorkforceManagement/views/Department_View.py
+++ WorkforceManagement/views/Department_View.py
@@ -5,3 +5,8 @@ def Department_List_View(request):
     """ Displays all Departments in the company """
     departments = Department.objects.all()
     return render(request, 'WorkforceManagement/Department_list.html', {'departments': departments})
+
+def Department_Detail_View(request, pk):
+    """Displays details about a specific department"""
+    department = get_object_or_404(department, pk=pk)
+    return render(request, 'WorkforceManagement/Department_Detail.html', {'department': department})
