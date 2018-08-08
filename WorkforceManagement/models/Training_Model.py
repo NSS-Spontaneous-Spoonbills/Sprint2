177df63001b141c4ddf145937e59110f19e5e759
dan beecroft <binauralbeat001@gmail.com>
2018-08-08 10:39:57 -0500

    added templates for training list and detail

--- WorkforceManagement/models/Training_Model.py
+++ WorkforceManagement/models/Training_Model.py
@@ -8,10 +8,10 @@ class Training_Prog(models.Model):
 
     prog_name = models.CharField(max_length=50)
     training_desc = models.CharField(max_length=100)
-    start_date = models.DateField(auto_now=True)
-    end_date = models.DateField(auto_now=False)
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    end_date = models.DateTimeField(auto_now=True, auto_now_add=False)
     max_attendance = models.CharField(max_length=50)
 
     def __str__(self):
 
-        return f'{self.prog_name}'
+        return f"ProgNum {self.id}"
--- WorkforceManagement/views/Training_View.py
+++ WorkforceManagement/views/Training_View.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, TemplateView, FormView, DetailView, CreateView
-from django import HttpResponse
+
 
 
 from WorkforceManagement.models import Training_Prog
@@ -8,4 +8,10 @@ from WorkforceManagement.models import Training_Prog
 def Training_List_View(request):
     """ Displays all training programs in the company """
     training_progs = Training_Prog.objects.all()
-    return render(request, 'WorkforceManagement/Training_list.html', {'training_progs': training_progs})
+    return render(request, 'WorkforceManagement/Training_List.html', {'training_progs': training_progs})
+
+
+def Training_Detail_View(request, pk):
+    """Displays details about a specific computer"""
+    prog = get_object_or_404(Training_Prog, pk=pk)
+    return render(request, 'WorkforceManagement/Training_Detail.html', {'prog': prog})
