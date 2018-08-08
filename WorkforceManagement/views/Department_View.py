bd8503d74ac2a6b953a883c79872b87ff24b8a83
David Paul <dfp333@gmail.com>
2018-08-08 14:42:18 -0500

    got the list of employees to print, need to test to make sure that it's making the right comparison

--- WorkforceManagement/views/Department_View.py
+++ WorkforceManagement/views/Department_View.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from WorkforceManagement.models import Department
+from WorkforceManagement.models import Department, Employee
 
 def Department_List_View(request):
     """ Displays all Departments in the company """
@@ -9,4 +9,5 @@ def Department_List_View(request):
 def Department_Detail_View(request, pk):
     """Displays details about a specific department"""
     department = get_object_or_404(Department, pk=pk)
-    return render(request, 'WorkforceManagement/Department_Detail.html', {'department': department})
+    employees = Employee.objects.all()
+    return render(request, 'WorkforceManagement/Department_Detail.html', {'department': department, 'employees': employees})
