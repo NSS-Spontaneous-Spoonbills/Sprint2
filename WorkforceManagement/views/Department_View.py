ca90858023a21b9a2d0622e00ffb71f977d12790
David Paul <dfp333@gmail.com>
2018-08-09 10:41:09 -0500

    added def Department_New_View to Departments_View.py

--- WorkforceManagement/views/Department_View.py
+++ WorkforceManagement/views/Department_View.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from WorkforceManagement.models import Department, Employee
+from WorkforceManagement.forms import *
 
 def Department_List_View(request):
-    """ Displays all Departments in the company """
+    """
+    Displays all Departments in the company
+    Author: David Paul
+    """
     departments = Department.objects.all()
     return render(request, 'WorkforceManagement/Department_list.html', {'departments': departments})
 
@@ -11,3 +15,18 @@ def Department_Detail_View(request, pk):
     department = get_object_or_404(Department, pk=pk)
     employees = Employee.objects.all()
     return render(request, 'WorkforceManagement/Department_Detail.html', {'department': department, 'employees': employees})
+
+def Department_New_View(request):
+    """Displays form for adding new computer to the database
+    Author: David Paul
+    """
+    if request.method == "POST":
+        form = Department_New_Form(request.POST)
+        new_comp = form.save(commit=False)
+        new_comp.save()
+        return redirect('computer_detail', pk=new_comp.pk)
+    else:
+        form = Department_New_Form()
+    return render(request, 'WorkforceManagement/Department_Update.html', {'form': form})
+
+
