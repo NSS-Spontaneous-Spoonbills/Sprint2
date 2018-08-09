9f1ea0462f6991f6f5dafae5bcd58df3fa8a5328
CashewRose <danieagnoletti@gmail.com>
2018-08-09 10:15:19 -0500

    Grabs specific employee information and plugs it into the template

--- WorkforceManagement/urls.py
+++ WorkforceManagement/urls.py
@@ -4,15 +4,16 @@ from WorkforceManagement.views import Employee_View
 
 
 urlpatterns = [
-    path('/computers/', views.Computer_List_View, name="computer_list"),
+    path('computers/', views.Computer_List_View, name="computer_list"),
     path('computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
     path('computers/new', views.Computer_New_View, name='computer_new'),
     path('computers/<int:pk>/update/',
          views.Computer_Update_View, name='computer_update'),
-    path('/employees/', views.Employee_View.Employee_List_View.as_view(), name='employee_list'),
-    path('/employees/<int:pk>', views.Employee_View.Employee_Detail_View.as_view(), name='employee_detail'),
-    path('/computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
+    path('employees/', views.Employee_View.Employee_List_View.as_view(), name='employee_list'),
+    path('employees/<int:pk>', views.Employee_View.Employee_Detail_View.as_view(), name='employee_detail'),
+    path('computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
     path('training_programs/', views.Training_List_View, name="training_list"),
     path('training_programs/<int:pk>/', views.Training_Detail_View, name="training_detail"),
-    path('/departments/', views.Department_List_View, name="department_list"),
-    path('/departments/<int:pk>/', views.Department_Detail_View, name="department_detail"),
+    path('departments/', views.Department_List_View, name="department_list"),
+    path('departments/<int:pk>/', views.Department_Detail_View, name="department_detail"),
+]
--- WorkforceManagement/views/Employee_View.py
+++ WorkforceManagement/views/Employee_View.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from WorkforceManagement.models import Department, Computer, Employee
 
 
@@ -14,7 +14,14 @@ class Employee_List_View(ListView):
     context_object_name = 'Employee_List'
     template_name = 'WorkforceManagement/Employee_List.html'
 
-class Employee_Detail_View(ListView):
+class Employee_Detail_View(DetailView):
+    """
+    Employee_Detail_View inherits DetailView and is using the Employee model and using the URL pk to find the specific Employee VIA id
+    *context_object_name is a method override to rename from 'object_list' --> 'Employee_Detail'
+    *template_name is a method that looks for an html file called 'Employee_Detail.html' to send information 
+        Author: Cashew Rose
+
+    """
     model = Employee
     context_object_name = 'Employee_Detail'
     template_name = 'WorkforceManagement/Employee_Detail.html'
