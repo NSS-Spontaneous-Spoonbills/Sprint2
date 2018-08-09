48741cecbf10d17317e7ebaa53522355decb2beb
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-09 09:41:46 -0500

    Removed backslash from urls

--- WorkforceManagement/urls.py
+++ WorkforceManagement/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path('computers/<int:pk>/', views.Computer_Detail_View, name="computer_detail"),
     path('computers/new', views.Computer_New_View, name='computer_new'),
     path('computers/<int:pk>/update/',
-         views.Computer_Update_View, name='computer_update'),
+        views.Computer_Update_View, name='computer_update'),
     path('employees/', views.Employee_View.Employee_List_View.as_view(),
-         name='Employee_List'),
+        name='Employee_List'),
     path('training_programs/', views.Training_List_View, name="training_list"),
     path('training_programs/<int:pk>/', views.Training_Detail_View, name="training_detail"),
     path('departments/', views.Department_List_View, name="department_list"),
-    path('/departments/<int:pk>/', views.Department_Detail_View, name="department_detail"),
+    path('departments/<int:pk>/', views.Department_Detail_View, name="department_detail"),
 
 ]
