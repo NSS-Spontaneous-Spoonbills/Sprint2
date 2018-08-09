679ff9e527527aa95d0dd4761e9223c3f60e6aed
dan beecroft <binauralbeat001@gmail.com>
2018-08-09 10:51:51 -0500

    commiting before reviewing another branch

--- WorkforceManagement/views/Training_View.py
+++ WorkforceManagement/views/Training_View.py
@@ -13,3 +13,17 @@ def Training_Detail_View(request, pk):
     """Displays details about a specific program"""
     prog = get_object_or_404(Training_Prog, pk=pk)
     return render(request, 'WorkforceManagement/Training_Detail.html', {'prog': prog})
+
+def Training_Edit_View(request, pk):
+    """Displays form for updating the computers
+    Author: Erin Meaker
+    """
+    computer = get_object_or_404(Computer, pk=pk)
+    if request.method == "POST":
+        form = Computer_Update_Form(request.POST, instance=computer)
+        computer = form.save(commit=False)
+        computer.save()
+        return redirect('computer_detail', pk=computer.pk)
+    else:
+        form = Computer_Update_Form(instance=computer)
+    return render(request, 'WorkforceManagement/Computer_Update.html', {'form': form})
