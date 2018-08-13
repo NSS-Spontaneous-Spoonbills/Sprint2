7ea0bf81104b7d0c4d81fd4140f21ed1aee498f8
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-13 09:20:47 -0500

    Fixed git merge issues

--- WorkforceManagement/forms/__init__.py
+++ WorkforceManagement/forms/__init__.py
@@ -1,7 +1,4 @@
 from .Computer_Form import Computer_New_Form
 from .Computer_Form import Computer_Update_Form
-<<<<<<< HEAD
 from .Employee_Form import Employee_New_Form
-=======
 from .Department_Form import Department_New_Form
->>>>>>> ab42325763f1d57523127f43d0bb593825288eb7
