ed0baffda285a031d4e91652c0cd9391d61d5f6b
MrErin <erinmeaker@gmail.com>
2018-08-13 10:07:22 -0500

    tests complete

--- WorkforceManagement/test/test_computers.py
+++ WorkforceManagement/test/test_computers.py
@@ -60,4 +60,10 @@ class Computer_Test(TestCase):
     def test_delete(self):
         """This test asserts that the assigned computer cannot be deleted and remains in the database but the unassigned computer is deleted"""
 
-        pass
+        self.delete_response_no_go = self.client.delete(
+            reverse('computer_delete', kwargs={'pk': 1}))
+        self.delete_response_yes_go = self.client.delete(
+            reverse('computer_delete', kwargs={'pk': 2}))
+
+        self.assertEqual(self.delete_response_no_go.status_code, 200)
+        self.assertEqual(self.delete_response_yes_go.status_code, 200)
