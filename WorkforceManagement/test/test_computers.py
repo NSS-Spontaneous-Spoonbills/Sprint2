5169eafc54ea9f9877ec50e2f9a381b1a01ef38d
MrErin <erinmeaker@gmail.com>
2018-08-13 09:35:21 -0500

    working on tests

--- WorkforceManagement/test/test_computers.py
+++ WorkforceManagement/test/test_computers.py
@@ -1,21 +1,26 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from WorkforceManagement.models import Computer_Model
+
+
 class Computer_Test(TestCase):
     """Tests computer functionality for WorkforceManagement
     Author: Erin Meaker
     """
 
-    def test_computer_setup(self):
+    def setup(self):
         """Tests will build two testing computers to ascertain the following:
 
         Author: Erin Meaker
         """
-        assigned_Computer = Computer_Model.Computer.objects.create(
+        self.assigned_Computer = Computer_Model.Computer.objects.create(
             purchase_date='2018-01-01',
             decom_date=None,
             manufacturer='Dell',
             make='Spider Nest',
             has_been_assigned='1'
         )
-        unassigned_Computer = Computer_Model.Computer.objects.create(
+        self.unassigned_Computer = Computer_Model.Computer.objects.create(
             purchase_date='2018-01-01',
             decom_date=None,
             manufacturer='Apple',
@@ -26,31 +31,33 @@ class Computer_Test(TestCase):
         """ass_response refers to the object returned when pulling an "assigned computer" from the data above. It is not a fart.
         Likewise, "unass_response" refers to the unassigned computer.
         """
-        ass_response = self.client.get(
+        self.ass_response = self.client.get(
             reverse('computer_detail', kwargs={'pk': 1}))
 
-        unass_response = self.client.get(
+        self.unass_response = self.client.get(
             reverse('computer_detail', kwargs={'pk': 2}))
 
     def test_status_code(self):
         """This test asserts that the computer object is returned. The status code 200 means everything is fine."""
-        self.assertEqual(ass_response.status_code, 200)
-        self.assertEqual(unass_response.status_code, 200)
+        self.assertEqual(self.ass_response.status_code, 200)
+        self.assertEqual(self.unass_response.status_code, 200)
 
     def test_data_is_correct(self):
-        """This test asserts that the text and bool fields from the computer object is appearing correctly in the returned data."""
-        self.assertIn(assigned_Computer.make.encode(),
-                      ass_response.content)
-        self.assertIn(unassigned_Computer.make.encode(),
-                      unass_response.content)
-        self.assertIn(assigned_Computer.manufacturer.encode(),
-                      ass_response.content)
-        self.assertIn(unassigned_Computer.manufacturer.encode(),
-                      unass_response.content)
-        self.assertIn(assigned_Computer.has_been_assigned.encode(),
-                      ass_response.content)
-        self.assertIn(unassigned_Computer.has_been_assigned.encode(),
-                      unass_response.content)
+        """This test asserts that the text and bool fields from the computer object appear correctly in the returned data."""
+        self.assertIn(self.assigned_Computer.make.encode(),
+                      self.ass_response.content)
+        self.assertIn(self.unassigned_Computer.make.encode(),
+                      self.unass_response.content)
+        self.assertIn(self.assigned_Computer.manufacturer.encode(),
+                      self.ass_response.content)
+        self.assertIn(self.unassigned_Computer.manufacturer.encode(),
+                      self.unass_response.content)
+        self.assertIn(self.assigned_Computer.has_been_assigned.encode(),
+                      self.ass_response.content)
+        self.assertIn(self.unassigned_Computer.has_been_assigned.encode(),
+                      self.unass_response.content)
 
     def test_delete(self):
         """This test asserts that the assigned computer cannot be deleted and remains in the database but the unassigned computer is deleted"""
+
+        pass
